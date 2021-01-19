fac4e66a78bfec1e216d845502c838a177e83b66
Sk-Aryan <77684868+Sk-Aryan@users.noreply.github.com>
2021-01-19 21:42:47 +0530

    Create proj1.py
    This file runs a code to find the area of a circle by taking the radius as input from the user.

---
+++ proj1.py
@@ -0,0 +1,5 @@
+PI = 3.14
+radius = float(input('Input the radius of the circle: '))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+print('The area of the circle with radius %d is: %.2f',area)
